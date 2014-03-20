a181cddc6ea9e544b8a791fb3829f8be61da376f
Theo Nicolaou <theo.nicolaou@capgemini.com>
2014-03-20 12:10:38 +0000

    Disable line comments in scss files

--- config.rb
+++ config.rb
@@ -19,8 +19,8 @@ output_style = :expanded
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-line_comments = true
+line_comments = false
+# line_comments = true
 
 
 # If you prefer the indented syntax, you might want to regenerate this
